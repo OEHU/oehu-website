8ca3dd46b643891730068f484965d78a3b8c8656
bartwr <mail@bartroorda.nl>
2018-09-26 23:35:17 +0200

    Redesigned <Social /> component

--- src/components/Social.vue
+++ src/components/Social.vue
@@ -1,67 +1,62 @@
 <template>
-    <div class="container content">
-        <div class="box">
-            <h3>Follow us</h3>
-            <div class="columns">
-                <div class="column"></div>
-                <div class="column">
-                    <span class="social-icon">
-                        <a href="https://t.me/joinchat/A8b03hI61nBIbnVF18582A"><i class="fab fa-telegram fa-2x"></i></a>
-                    </span>
-                </div>
-                <div class="column">
-                    <span class="social-icon">
-                        <a href="https://www.linkedin.com/company/11865484/admin/overview/"><i
-                                class="fab fa-linkedin fa-2x"></i></a>
-                    </span>
-                </div>
-                <div class="column">
-                    <span class="social-icon">
-                        <a href="https://twitter.com/oehu_project"><i class="fab fa-twitter fa-2x"></i></a>
-                    </span>
-                </div>
-                <div class="column"></div>
-            </div>
+    <div class="Social">
 
-            <!-- Begin MailChimp Signup Form -->
-            <div id="mc_embed_signup">
-                <form action="https://oehu.us19.list-manage.com/subscribe/post?u=ab502529571629d74b3510e56&amp;id=dacb068b56"
-                      method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form"
-                      class="validate"
-                      target="_blank" novalidate>
-                    <div id="mc_embed_signup_scroll">
-                        <h3 class="">Subscribe to our mailing list</h3>
-                        <div class="mc-field-group">
-                            <label for="mce-EMAIL"></label>
-                            <input type="email" value="" name="EMAIL" class="required email input"
-                                   placeholder="Email Address" id="mce-EMAIL">
-                        </div>
+        <h1>Join our community!</h1>
 
-                        <div id="mce-responses" class="clear">
-                            <div class="response" id="mce-error-response" style="display:none"></div>
-                            <div class="response" id="mce-success-response" style="display:none"></div>
-                        </div>
-                        <div style="position: absolute; left: -5000px;" aria-hidden="true">
-                            <input type="text" name="b_ab502529571629d74b3510e56_dacb068b56" tabindex="-1"
-                                   value="">
-                        </div>
-                        <div class="clear"><input type="submit" value="Subscribe" name="subscribe"
-                                                  id="mc-embedded-subscribe" class="button is-black"></div>
-                    </div>
-                </form>
-            </div>
-            <!--End mc_embed_signup-->
+        <p>
+            Stay up to date on OEHU's developments by subscribing to our newsletter or follow us on one of the channels below
+        </p>
 
+        <NewsletterSubscribe />
+
+        <div class="social-icons flex center">
+            <span class="social-icon">
+                <a href="https://twitter.com/oehu_project" target="_blank">
+                    <img src="./Social/Twitter.png" />
+                </a>
+            </span>
+            <span class="social-icon">
+                <a href="https://www.linkedin.com/company/11865484/admin/overview/" target="_blank">
+                    <img src="./Social/LinkedIn.png" />
+                </a>
+            </span>
+            <span class="social-icon">
+                <a href="https://t.me/joinchat/A8b03hI61nBIbnVF18582A" target="_blank">
+                    <img src="./Social/Telegram.png" />
+                </a>
+            </span>
         </div>
     </div>
 </template>
 
 <script>
+    import NewsletterSubscribe from '@/components/NewsletterSubscribe.vue';
+
     export default {
-        name: 'Social',
+        components: {NewsletterSubscribe},
+        name: 'Social'
     }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+.Social {
+    background: #fff;
+    color: #333;
+    text-align: center;
+    padding: 25px 0;
+}
+.Social h1 {
+    font-size: 40px;
+    font-family: serif;
+}
+.Social h1,
+.Social p {
+    max-width: 65vw;
+    margin: 0 auto;
+}
+.social-icon {
+    max-width: 60px;
+    margin: 5px;
+}
 </style>
