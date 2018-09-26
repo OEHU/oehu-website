ad57903fb2ad82771fc29481fb3a5765ecf6c234
bartwr <mail@bartroorda.nl>
2018-09-26 23:34:41 +0200

    Created <Button /> component

---
+++ src/components/Button.vue
@@ -0,0 +1,23 @@
+<template>
+    <button>
+        {{this.title}}
+    </button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: ['title'],
+    }
+</script>
+
+<style scoped lang="scss">
+
+button {
+    background: transparent;
+    border: solid #fff 1px;
+    padding: 8px 12px;
+    color: #fff;
+}
+
+</style>
