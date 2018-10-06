aafe52c42e5b8c51ef03accda69ba02fbe72c516
bartwr <mail@bartroorda.nl>
2018-10-06 20:11:46 +0200

    Matched tagline to Sketch design

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -5,7 +5,7 @@
             <img src="../assets/images/logo/Logo.png"/>
         </div>
 
-        <h2 class="tagline is-size-1">
+        <h2 class="tagline">
             Sharing energy data for the better
         </h2>
     </div>
@@ -27,20 +27,40 @@
         text-align: center;
 
         .tagline {
-            margin: 0;
-            text-align: center;
+            position: relative;
+            z-index: 1;
+
+            margin: 0 auto;
+
+            line-height: 35px;
+            font-size: 24px;
+            font-weight: 400;
             color: #000;
+
+            width: 280px;
+            max-width: 100%;
+
+            @media (min-width: 480px) {
+                font-size: 3rem;
+                line-height: 4rem;
+
+                width: 1100px;
+                max-width: 80%;
+            }
         }
 
         img {
-            max-width: 70%;
+            width: 275px;
+            max-width: 80%;
             margin: 40px auto;
             display: block;
 
             @include minimum-desktop() {
+                width: auto;
                 max-width: 480px;
             }
             @include tablet() {
+                width: auto;
                 max-width: 40%;
             }
         }
