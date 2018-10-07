df2d52b3cbce68ce80d336027dc32fda882cb939
bartwr <mail@bartroorda.nl>
2018-10-07 23:39:04 +0200

    Updated (example) deployment script

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,18 @@
+# Set node version
+nvm use 9.0
+# Run build
+npm run build
 # Empty build folder
 rm -R ../oehu-website-build/*
 # Move new build
 mv ./build-copy/* ../oehu-website-build/
+
+# Go to build
+cd ../oehu-website-build/
 # Create CNAME file
 echo "oehu.org" > CNAME
-# Git push
-cd ../oehu-website-build/
+# Add all files + commit
 git add *
 git commit -m "New build"
+# Push
 git push -ff
