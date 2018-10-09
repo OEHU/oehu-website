855bc50875ceabbe2c7a5d5af7af8339bd2686a3
Marijn Bent <marijnbent@gmail.com>
2018-10-09 09:48:34 +0200

    Basic icon

--- src/components/home/Maps.vue
+++ src/components/home/Maps.vue
@@ -11,9 +11,7 @@
         <l-marker
                 v-for="marker in markers"
                 :key="marker.id"
-                :lat-lng="marker.position"
-                @click="alert(marker)">
-
+                :lat-lng="marker.position">
             <!--<l-popup :content="marker.tooltip" />-->
             <l-tooltip :content="marker.tooltip" />
 
@@ -23,11 +21,13 @@
 
 <script>
     import { LMap, LTileLayer, LMarker, LTooltip } from 'vue2-leaflet';
+    import L from 'leaflet';
     import axios from 'axios';
 
     export default {
-        name: 'Example',
+        name: 'Maps',
         components: {
+            L,
             LMap,
             LTileLayer,
             LMarker,
@@ -38,8 +38,18 @@
                 zoom: 10,
                 center: {lat: 52.0182305, lng: 4.6910549},
                 url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+                icon: '@/images/maps/marker-icon.png',
                 attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
                 markers: [],
+                LIcon : L.icon({
+                    iconUrl: this.icon,
+                    iconSize: [38, 95],
+                    iconAnchor: [22, 94],
+                    popupAnchor: [-3, -76],
+                    shadowUrl: 'my-icon-shadow.png',
+                    shadowSize: [68, 95],
+                    shadowAnchor: [22, 94]
+                })
             };
         },
         methods: {
@@ -61,12 +71,11 @@
                 devices.forEach((device) => {
                     this.markers.push({
                         id: Math.floor((Math.random() * 1000) + 1),
-                        // id: device.id,
+                        icon: this.icon,
                         position: {lat: device.device.location.coordinates[0], lng: device.device.location.coordinates[1]},
                         tooltip: 'Totaal verbruikt: ' + device.device.electricityReceived.total
                     })
                 })
-                console.log(markers);
             }
         },
         mounted() {
--- src/main.js
+++ src/main.js
@@ -11,7 +11,15 @@ import 'vue-form-wizard/dist/vue-form-wizard.min.css'
 import VueFormGenerator from 'vue-form-generator'
 
 //Maps
-import "leaflet/dist/leaflet.css"
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+delete L.Icon.Default.prototype._getIconUrl
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+})
 
 Vue.config.productionTip = false
 Vue.use(VueFormWizard)
