ddcd379b59527bd1944965e6b534e1b7a565b4fc
Marijn Bent <marijnbent@gmail.com>
2018-10-16 15:16:46 +0200

    Setup: Partial new design added

--- src/views/Setup.vue
+++ src/views/Setup.vue
@@ -70,276 +70,311 @@
 </template>
 
 <script>
-import VueFormGenerator from "vue-form-generator";
-import "vue-form-generator/dist/vfg-core.css";
-import { LMap, LTileLayer, LMarker, LCircle } from "vue2-leaflet";
-import L from "leaflet";
+    import VueFormGenerator from "vue-form-generator";
+    import "vue-form-generator/dist/vfg-core.css";
+    import {LMap, LTileLayer, LMarker, LCircle} from "vue2-leaflet";
+    import L from "leaflet";
 
-export default {
-  components: {
-    LMap,
-    LTileLayer,
-    LMarker,
-      LCircle
-  },
-  data: function() {
-    return {
-      deviceId: 0,
-      phrase: 0,
-        map: {
-            marker: {
-                position: {
-                    lat: 0,
-                    lng: 0
-                },
-                radius: 10000
-            },
-            zoom: 10,
-            currentZoom: 10,
-            center: {lat: 52.0182305, lng: 4.6910549},
-            currentCenter: {lat: 52.0182305, lng: 4.6910549},
-            url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
-            attribution:
-                '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+    export default {
+        components: {
+            LMap,
+            LTileLayer,
+            LMarker,
+            LCircle
         },
-        LIcon: L.icon({
-            iconUrl: "/marker-icon.png",
-            iconRetinaUrl: "/marker-icon-2x.png",
-            iconSize: [25, 25],
-      }),
-      model: {
-          lat: "",
-          long: "",
-          accuracy: 1000
-      },
-      backupTab: {
-        fields: [
-          {
-            type: "input",
-            inputType: "text",
-            label: "Phrase:",
-            required: true,
-            default: this.phrase,
-            model: "phrase",
-            validator: VueFormGenerator.validators.string
-          },
-          {
-            type: "input",
-            inputType: "text",
-            label: "Check email",
-            model: "email",
-            required: true,
-            validator: VueFormGenerator.validators.string
-          },
-          {
-            type: "input",
-            inputType: "password",
-            label: "Check password ",
-            model: "password",
-            required: true,
-            validator: VueFormGenerator.validators.string
-          }
-        ]
-      },
-      formOptions: {
-        validationErrorClass: "has-error",
-        validationSuccessClass: "has-success",
-        validateAfterChanged: true
-      },
-      selectBuilding: {
-        fields: [
-          {
-            type: "select",
-            label: "Select building type:",
-            model: "building",
-            required: true,
-            validator: VueFormGenerator.validators.string,
-            values: ["House", "Office", "Storage", "Factory"],
-            selectOptions: {
-              noneSelectedText: "Select type"
+        data: function () {
+            return {
+                deviceId: 0,
+                phrase: 0,
+                map: {
+                    marker: {
+                        position: {
+                            lat: 0,
+                            lng: 0
+                        },
+                        radius: 10000
+                    },
+                    zoom: 10,
+                    currentZoom: 10,
+                    center: {lat: 52.0182305, lng: 4.6910549},
+                    currentCenter: {lat: 52.0182305, lng: 4.6910549},
+                    url: "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+                    attribution:
+                        '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+                },
+                LIcon: L.icon({
+                    iconUrl: "/marker-icon.png",
+                    iconRetinaUrl: "/marker-icon-2x.png",
+                    iconSize: [25, 25],
+                }),
+                model: {
+                    lat: "",
+                    long: "",
+                    accuracy: 1000
+                },
+                backupTab: {
+                    fields: [
+                        {
+                            type: "input",
+                            inputType: "text",
+                            label: "Phrase:",
+                            required: true,
+                            default: this.phrase,
+                            model: "phrase",
+                            validator: VueFormGenerator.validators.string
+                        },
+                        {
+                            type: "input",
+                            inputType: "text",
+                            label: "Check email",
+                            model: "email",
+                            required: true,
+                            validator: VueFormGenerator.validators.string
+                        },
+                        {
+                            type: "input",
+                            inputType: "password",
+                            label: "Check password ",
+                            model: "password",
+                            required: true,
+                            validator: VueFormGenerator.validators.string
+                        }
+                    ]
+                },
+                formOptions: {
+                    validationErrorClass: "has-error",
+                    validationSuccessClass: "has-success",
+                    validateAfterChanged: true
+                },
+                selectBuilding: {
+                    fields: [
+                        {
+                            type: "select",
+                            label: "Select building type:",
+                            model: "building",
+                            required: true,
+                            validator: VueFormGenerator.validators.string,
+                            values: ["House", "Office", "Storage", "Factory"],
+                            selectOptions: {
+                                noneSelectedText: "Select type"
+                            }
+                        },
+                        {
+                            type: "select",
+                            label: "Select occupants:",
+                            model: "occupants",
+                            validator: VueFormGenerator.validators.string,
+                            required: true,
+                            values: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
+                            selectOptions: {
+                                noneSelectedText: "Amount of occupants"
+                            }
+                        }
+                    ]
+                },
+                selectLocation: {
+                    fields: [
+                        {
+                            type: "input",
+                            inputType: "number",
+                            label: "Location Accuracy",
+                            model: "accuracy",
+                            min: 100,
+                            required: true,
+                            validator: VueFormGenerator.validators.string,
+                            styleClasses: "col-xs-6"
+                        },
+                        {
+                            type: "input",
+                            inputType: "text",
+                            label: "Latitude",
+                            model: "lat",
+                            required: true,
+                            validator: VueFormGenerator.validators.string,
+                            styleClasses: "col-xs-6"
+                        },
+                        {
+                            type: "input",
+                            inputType: "text",
+                            label: "Longitude",
+                            model: "long",
+                            required: true,
+                            validator: VueFormGenerator.validators.string,
+                            styleClasses: "col-xs-6"
+                        }
+                    ]
+                },
+                credentialsTab: {
+                    fields: [
+                        {
+                            type: "input",
+                            inputType: "text",
+                            label: "email:",
+                            model: "email",
+                            required: true,
+                            validator: VueFormGenerator.validators.string
+                        },
+                        {
+                            type: "input",
+                            inputType: "password",
+                            label: "Password:",
+                            model: "password",
+                            required: true,
+                            validator: VueFormGenerator.validators.string
+                        }
+                    ]
+                }
+            };
+        },
+        methods: {
+            validateBuildingTab: function () {
+                this.registerNewDevice();
+                return this.$refs.selectBuilding.validate();
+            },
+            validateCredentialsTab: function () {
+                this.registerAccount();
+                this.loadPhrase();
+                return this.$refs.credentialsTab.validate();
+            },
+            validateBackupTab: function () {
+                return this.$refs.backupTab.validate();
+            },
+            zoomUpdate(zoom) {
+                this.map.currentZoom = zoom;
+            },
+            centerUpdate(center) {
+                this.map.currentCenter = center;
+            },
+            markerClick(e) { //todo: can be a bit cleaner
+                this.map.marker.position.lat = e.latlng.lat;
+                this.map.marker.position.lng = e.latlng.lng;
+                this.model.lat = this.map.marker.position.lat;
+                this.model.long = this.map.marker.position.lng;
+            },
+            getConfigurated() {
+                this.axios
+                .get("https://localhost:8000/oehu/GetConfigurated")
+                .then(function (response) {
+                    if (response.data.configurated !== true) {
+                        this.generateNewPhrase();
+                    } else {
+                        //redirect to dashboard
+                    }
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            },
+            generateNewPhrase() {
+                this.axios
+                .get("https://localhost:8000/oehu/GenerateNewPhrase")
+                .then(function (response) {
+                    this.phrase = response.data.phrase;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            },
+            registerNewDevice() {
+                //na 2
+                this.axios
+                .get(
+                    "https://localhost:8000/oehu/registerDevice/" +
+                    "OEHU" +
+                    "/" +
+                    this.model.lat +
+                    "/" +
+                    this.model.long +
+                    "/" +
+                    this.model.accuracy +
+                    "/" +
+                    this.model.building +
+                    "/" +
+                    this.model.occupants
+                )
+                .then(function (response) {
+                    this.deviceId = response.data.deviceID;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            },
+            registerAccount() {
+                //na 3
+                this.axios
+                .post("https://api.oehu.org/account/register", {
+                    email: this.model.email,
+                    password: this.model.password,
+                    deviceId: this.deviceId
+                })
+                .then(function (response) {
+                    console.log(response);
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
+            },
+            loadPhrase: function () {
+                this.model = VueFormGenerator.schema.createDefaultObject(this.backupTab);
+            },
+            mounted() {
+                this.getConfigurated();
             }
-          },
-          {
-            type: "select",
-            label: "Select occupants:",
-            model: "occupants",
-            validator: VueFormGenerator.validators.string,
-            required: true,
-            values: ["1", "2", "3", "4", "5", "6", "7", "8", "9"],
-            selectOptions: {
-              noneSelectedText: "Amount of occupants"
-            }
-          }
-        ]
-      },
-      selectLocation: {
-        fields: [
-          {
-            type: "input",
-            inputType: "number",
-            label: "Location Accuracy",
-            model: "accuracy",
-            min: 100,
-            required: true,
-            validator: VueFormGenerator.validators.string,
-            styleClasses: "col-xs-6"
-          },
-          {
-            type: "input",
-            inputType: "text",
-            label: "Latitude",
-            model: "lat",
-            required: true,
-            validator: VueFormGenerator.validators.string,
-            styleClasses: "col-xs-6"
-          },
-          {
-            type: "input",
-            inputType: "text",
-            label: "Longitude",
-            model: "long",
-            required: true,
-            validator: VueFormGenerator.validators.string,
-            styleClasses: "col-xs-6"
-          }
-        ]
-      },
-      credentialsTab: {
-        fields: [
-          {
-            type: "input",
-            inputType: "text",
-            label: "email:",
-            model: "email",
-            required: true,
-            validator: VueFormGenerator.validators.string
-          },
-          {
-            type: "input",
-            inputType: "password",
-            label: "Password:",
-            model: "password",
-            required: true,
-            validator: VueFormGenerator.validators.string
-          }
-        ]
-      }
+        }
     };
-  },
-  methods: {
-    validateBuildingTab: function() {
-      this.registerNewDevice();
-      return this.$refs.selectBuilding.validate();
-    },
-    validateCredentialsTab: function() {
-      this.registerAccount();
-      this.loadPhrase();
-      return this.$refs.credentialsTab.validate();
-    },
-    validateBackupTab: function() {
-      return this.$refs.backupTab.validate();
-    },
-    zoomUpdate(zoom) {
-      this.map.currentZoom = zoom;
-    },
-    centerUpdate(center) {
-      this.map.currentCenter = center;
-    },
-      markerClick(e) { //todo: can be a bit cleaner
-        this.map.marker.position.lat = e.latlng.lat;
-        this.map.marker.position.lng = e.latlng.lng;
-        this.model.lat = this.map.marker.position.lat;
-        this.model.long = this.map.marker.position.lng;
-      },
-    getConfigurated() {
-      this.axios
-        .get("https://localhost:8000/oehu/GetConfigurated")
-        .then(function(response) {
-          if (response.data.configurated !== true) {
-            this.generateNewPhrase();
-          } else {
-            //redirect to dashboard
-          }
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    },
-    generateNewPhrase() {
-      this.axios
-        .get("https://localhost:8000/oehu/GenerateNewPhrase")
-        .then(function(response) {
-          this.phrase = response.data.phrase;
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    },
-    registerNewDevice() {
-      //na 2
-      this.axios
-        .get(
-          "https://localhost:8000/oehu/registerDevice/" +
-            "OEHU" +
-            "/" +
-            this.model.lat +
-            "/" +
-            this.model.long +
-            "/" +
-            this.model.accuracy +
-            "/" +
-            this.model.building +
-            "/" +
-            this.model.occupants
-        )
-        .then(function(response) {
-          this.deviceId = response.data.deviceID;
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    },
-    registerAccount() {
-      //na 3
-      this.axios
-        .post("https://api.oehu.org/account/register", {
-          email: this.model.email,
-          password: this.model.password,
-          deviceId: this.deviceId
-        })
-        .then(function(response) {
-          console.log(response);
-        })
-        .catch(function(error) {
-          console.log(error);
-        });
-    },
-    loadPhrase: function() {
-      this.model = VueFormGenerator.schema.createDefaultObject(this.backupTab);
-    },
-    mounted() {
-      this.getConfigurated();
-    }
-  }
-};
 </script>
 
 <style lang="scss">
-.container {
-  height: 1090px;
-  h1 {
-    font-size: 30px;
-  }
-  fieldset {
-    border: 0;
-  }
-  .title{
-      color: black;
-  }
-}
+    @import '../assets/sass/mix.scss';
 
-.tab {
-  height: 450px;
-}
+    .container {
+        height: 1090px;
+        h1 {
+            font-size: 30px;
+        }
+        fieldset {
+            border: 0;
+        }
+        .title {
+            color: black;
+        }
+
+        .vue-form-wizard {
+            .wizard-icon-container {
+                display: none;
+            }
+
+            .wizard-icon-circle {
+                background-color: #ffffff !important;
+                border: 20px solid $lightgray !important;
+                padding: 0 !important;
+                height: 150px !important;
+                width: 150px !important;
+                border-radius: 50% !important;
+
+                i {
+                    color: $lightgray !important;
+                    font-size: 30px !important;
+                }
+
+                &.checked {
+                    border-color: $black !important;
+                    background-color: $yellow !important;
+
+                    .wizard-icon-container {
+                        background-color: unset !important;
+                    }
+
+                    i {
+                        color: black !important;
+                    }
+                }
+            }
+        }
+
+        .tab {
+            height: 450px;
+        }
+    }
 </style>
 
