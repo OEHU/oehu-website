78756a6a8eef8ee656367d9c9bea9b0ceb5cb867
bartwr <mail@bartroorda.nl>
2018-11-15 21:43:09 +0100

    Only show useful data in the Maps data explorer + Updated tooltip

--- src/components/home/Maps.vue
+++ src/components/home/Maps.vue
@@ -14,7 +14,7 @@
                 :key="marker.id"
                 :icon="LIcon"
                 :lat-lng="marker.position">
-            <!--<l-popup :content="marker.tooltip" />-->
+            <!-- <l-popup :content="marker.tooltip" /> -->
             <l-tooltip :content="marker.tooltip" />
         </l-marker>
     </l-map>
@@ -22,8 +22,12 @@
 
 <script>
     import { LMap, LTileLayer, LMarker, LTooltip } from 'vue2-leaflet';
+    import moment from 'moment';
     import L from 'leaflet';
     import axios from 'axios';
+
+    let oneMonthAgo = moment().subtract(1, 'month').valueOf();
+    let oneWeekAgo = moment().subtract(1, 'week').valueOf();
 
     export default {
         name: 'Maps',
@@ -38,16 +42,12 @@
                 zoom: 10,
                 center: {lat: 52.0182305, lng: 4.6910549},
                 // Found some of the map styles below style on https://wiki.openstreetmap.org/wiki/Tiles#Servers
-                // url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-                // url: 'https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_all/{z}/{x}/{y}.png',
-                // url: 'https://{s}.tile.thunderforest.com/transport-dark/{z}/{x}/{y}.png',
                 url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner/{z}/{x}/{y}{r}.png',
                 attribution: '&copy; <a target="_blank" href="http://osm.org/copyright">OpenStreetMap</a> contributors',
                 markers: [],
                 LIcon: L.icon({
                     iconUrl: '/marker-icon.png',
                     iconRetinaUrl: '/marker-icon-2x.png',
-                    // shadowUrl: 'https://static.afbe+eldinguploaden.nl/1810/478987/xmrMaSzC.png',
                     iconSize: [50, 50],
                     iconAnchor: [25, 25],
                     popupAnchor: [0, 0],
@@ -72,11 +72,21 @@
             handleDevicesData(devices) {
                 devices.forEach((device) => {
                     if(! device.metadata ) return;
+                    if(! device.metadata.metadata ) return;
                     if(! device.metadata.metadata.householdType ) return;
+                    if(device.metadata.metadata.lastUpdate < oneWeekAgo) return;
                     this.markers.push({
                         id: device.deviceId,
                         position: {lat: device.metadata.metadata.location.coordinates[0], lng: device.metadata.metadata.location.coordinates[1]},
-                        tooltip: '' + device.metadata.metadata.householdType.charAt(0).toUpperCase() + device.metadata.metadata.householdType.slice(1)
+                        tooltip:
+                            ''
+                            + '<b>'+device.metadata.metadata.householdType+'</b><br />'
+                            + '<p><b>'+device.metadata.metadata.occupants+' occupants</b></p>'
+                            + '<hr />'
+                            + '<p>'+device.metadata.metadata.electricityReceived.total+' kWh received in total</p>'
+                            + '<p>'+device.metadata.metadata.electricityDelivered.total+' kWh delivered in total</p>'
+                            + '<p>'+device.metadata.metadata.gasReceived+' gas received in total</p>'
+                            + '<hr /><small>'+device.deviceId+'</small>'
                     })
                 })
             }
